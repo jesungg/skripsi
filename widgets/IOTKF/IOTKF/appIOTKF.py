1e36ed1fd4697c3fca4f2c550ac8ba179bfffa3b
Jessica Sung <jesung@Jessicas-MacBook-Air.local>
2020-12-18 04:28:34 +0700

    lagi di show settings, json udh oke, tinggal obdet dan kfnya

--- widgets/IOTKF/IOTKF/appIOTKF.py
+++ widgets/IOTKF/IOTKF/appIOTKF.py
@@ -1,4 +1,5 @@
 #other lib
+import json
 import os
 from pdb import run
 
@@ -6,12 +7,14 @@ import cv2
 import imutils
 import kivy
 import numpy as np
+#from Cython.Compiler.Naming import self_cname
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.factory import Factory
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 #uix lib
+from kivy.uix.togglebutton import ToggleButton, ToggleButtonBehavior
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
@@ -19,6 +22,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen, ScreenManager
+#from pylint import message
 
 kivy.require('1.11.1')
 
@@ -53,7 +57,7 @@ class selVidScr(Screen): #2
         vidcap = cv2.VideoCapture(fileloc)
         success, image = vidcap.read()
         if success:
-            cv2.imwrite("widgets/IOTKF/IOTKF/res/first_frame.jpg", image)  # save frame as JPEG file
+            cv2.imwrite("widgets/IOTKF/IOTKF/res/first_frame.png", image)  # save frame as JPEG file
     def select(self, dirpath, filepath):
         #print('path', dirpath,type(dirpath))
         #print('filename', filepath, type(filepath))
@@ -79,7 +83,33 @@ class selVidScr(Screen): #2
     
 
 class chooseSet(Screen): #3
-    pass
+    data={}
+    def pulldata(self):
+        try:
+            data=newSet().openJson()
+            #print(data)
+            #print(data['1']['namaset'])
+            #atas ini udh oke
+            #ih bego jgn pake dict[n], dia return nya smua dicnya 
+            # weeehhhh, l
+            # u kan cuman mau n nya weh 
+            if data is None:
+                print('no data')
+            if data is not None:
+                print('ada data')
+                for n in range(len(data)):
+                    pointer=str(n+1)
+                    print(data[pointer],(data[pointer]["namaset"]) )
+                    self.add_widget(ToggleButton(id=data[pointer], text=data[pointer]+" "+data[pointer]["namaset"]))
+                    if self.ids.data[pointer].state=='down':
+                        pass
+                    else:
+                        pass
+            else:
+                print('no data')
+        except Exception as e:
+            print(e)
+
 class newSet(Screen): #4
     def show_coor(self):
         events = [i for i in dir(cv2) if 'EVENT' in i]
@@ -103,13 +133,116 @@ class newSet(Screen): #4
                 cv2.imshow("image", img)
         #Here, you need to change the image name and it's path according to your directory
         font = cv2.FONT_HERSHEY_SIMPLEX
-        img = cv2.imread("/Users/jesung/Documents/code/skripsi2/skripsi/widgets/IOTKF/IOTKF/sampeltest.png")
+        img = cv2.imread("/Users/jesung/Documents/code/skripsi2/skripsi/widgets/IOTKF/IOTKF/res/first_frame.png")
         img = imutils.resize(img, width=600)
         cv2.imshow("image", img)
         # #calling the mouse click event
         cv2.setMouseCallback("image", click_event)
         if cv2.waitKey(0) == 27:  # if key 'q' is pressed 
             cv2.destroyWindow("image")
+    
+    def writeJson(self,dict):
+        with open("/Users/jesung/Documents/code/skripsi2/skripsi/widgets/IOTKF/IOTKF/res/data.json", "w") as outfile: 
+            json.dump(dict, outfile) 
+
+    def openJson(self):
+        f = open('/Users/jesung/Documents/code/skripsi2/skripsi/widgets/IOTKF/IOTKF/res/data.json',)
+        data = json.load(f)
+        print('you did it')
+        return data
+        
+    def get_variables(self):
+        data = {}
+        try:
+            self.openJson()
+        except Exception as e:
+            print (e)
+
+        setName = self.ids.setup_name.text
+        nLoc = self.ids.noise_loc.text
+        nCode = self.ids.noise_code.text
+
+        a1name = self.ids.a1nem.text
+        a2name = self.ids.a2nem.text
+        a3name = self.ids.a3nem.text
+        a4name = self.ids.a4nem.text
+        a5name = self.ids.a5nem.text
+        a6name = self.ids.a6nem.text
+        a7name = self.ids.a7nem.text
+        a8name = self.ids.a8nem.text
+        a9name = self.ids.a9nem.text
+        a10name = self.ids.a10nem.text
+
+        a1x0 = self.ids.a1xnol.text
+        a2x0 = self.ids.a2xnol.text
+        a3x0 = self.ids.a3xnol.text
+        a4x0 = self.ids.a4xnol.text
+        a5x0 = self.ids.a5xnol.text
+        a6x0 = self.ids.a6xnol.text
+        a7x0 = self.ids.a7xnol.text
+        a8x0 = self.ids.a8xnol.text
+        a9x0 = self.ids.a9xnol.text
+        a10x0 = self.ids.a10xnol.text
+        a1y0 = self.ids.a1ynol.text
+        a2y0 = self.ids.a2ynol.text
+        a3y0 = self.ids.a3ynol.text
+        a4y0 = self.ids.a4ynol.text
+        a5y0 = self.ids.a5ynol.text
+        a6y0 = self.ids.a6ynol.text
+        a7y0 = self.ids.a7ynol.text
+        a8y0 = self.ids.a8ynol.text
+        a9y0 = self.ids.a9ynol.text
+        a10y0 = self.ids.a10ynol.text
+
+        a1x1 = self.ids.a1xsatu.text
+        a2x1 = self.ids.a2xsatu.text
+        a3x1 = self.ids.a3xsatu.text
+        a4x1 = self.ids.a4xsatu.text
+        a5x1 = self.ids.a5xsatu.text
+        a6x1 = self.ids.a6xsatu.text
+        a7x1 = self.ids.a7xsatu.text
+        a8x1 = self.ids.a8xsatu.text
+        a9x1 = self.ids.a9xsatu.text
+        a10x1 = self.ids.a10xsatu.text
+        a1y1 = self.ids.a1ysatu.text
+        a2y1 = self.ids.a2ysatu.text
+        a3y1 = self.ids.a3ysatu.text
+        a4y1 = self.ids.a4ysatu.text
+        a5y1 = self.ids.a5ysatu.text
+        a6y1 = self.ids.a6ysatu.text
+        a7y1 = self.ids.a7ysatu.text
+        a8y1 = self.ids.a8ysatu.text
+        a9y1 = self.ids.a9ysatu.text
+        a10y1 = self.ids.a10ysatu.text
+
+        d_act = {'a1':{'name':a1name ,'x0':a1x0 ,'y0':a1y0 ,'x1':a1x1 ,'y1':a1y1},
+                'a2':{'name':a2name ,'x0':a2x0 ,'y0':a2y0 ,'x1':a2x1 ,'y1':a2y1},
+                'a3':{'name':a3name ,'x0':a3x0 ,'y0':a3y0 ,'x1':a3x1 ,'y1':a3y1},
+                'a4':{'name':a4name ,'x0':a4x0 ,'y0':a4y0 ,'x1':a4x1 ,'y1':a4y1},
+                'a5':{'name':a5name ,'x0':a5x0 ,'y0':a5y0 ,'x1':a5x1 ,'y1':a5y1},
+                'a6':{'name':a6name ,'x0':a6x0 ,'y0':a6y0 ,'x1':a6x1 ,'y1':a6y1},
+                'a7':{'name':a7name ,'x0':a7x0 ,'y0':a7y0 ,'x1':a7x1 ,'y1':a7y1},
+                'a8':{'name':a8name ,'x0':a8x0 ,'y0':a8y0 ,'x1':a8x1 ,'y1':a8y1},
+                'a9':{'name':a9name ,'x0':a9x0 ,'y0':a9y0 ,'x1':a9x1 ,'y1':a9y1},
+                'a10':{'name':a10name ,'x0':a10x0 ,'y0':a10y0 ,'x1':a10x1 ,'y1':a10y1}}
+        
+        if data is not None:
+            n_data = len(data)
+            in_id = n_data+1
+            d_setting = {in_id:{'namaset': setName,
+                            'nloc': nLoc,
+                            'ncod': nCode,
+                            'acts': d_act}}
+            self.writeJson(d_setting)
+        else:
+            d_setting = {1:{'namaset': setName,
+                            'nloc': nLoc,
+                            'ncod': nCode,
+                            'acts': d_act}}
+            self.writeJson(d_setting)
+        
+        print (nLoc,nCode)
+        
     
 
     
@@ -118,7 +251,7 @@ class cNoise(FloatLayout): #pop2
 class addAct(FloatLayout): #pop3
     pass
 class loadingScr(Screen): #5
-    pass
+     pass
 class resultScr(Screen): #6
     pass
 class resultVid(FloatLayout): #pop4
